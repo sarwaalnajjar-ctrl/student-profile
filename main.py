6e45ab13d16d76536496dab9d6a41fad30ecee40
sarwa alnajjar <sarwaalnajjar@gmail.com>
2026-09-14 22:01:01 +0300

    Add more student information

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 name = "Sarwa"
 major = "Data Science and AI"
 university = "University College of Applied Sciences"
+graduation_year = 2028 
 
 print("Student Profile")
 print("----------------")
 print("Name:", name)
 print("Major:", major)
 print("University:", university)
+print("Graduation Year:", graduation_year)
