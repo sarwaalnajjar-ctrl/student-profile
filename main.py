b53d39d30158f36569dc2d84f2cc3263e219963f
sarwa alnajjar <sarwaalnajjar@gmail.com>
2026-09-14 21:52:13 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "Sarwa"
+major = "Data Science and AI"
+university = "University College of Applied Sciences"
+
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
